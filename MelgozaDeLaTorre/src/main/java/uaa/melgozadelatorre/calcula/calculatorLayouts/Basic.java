3b5b3518b77dbf52234c89bc55a7a1a80bb43749
Eriarer <eriarer_messiah@hotmail.com>
2023-05-05 16:32:25 -0600

    calculator

---
+++ MelgozaDeLaTorre/src/main/java/uaa/melgozadelatorre/calcula/calculatorLayouts/Basic.java
@@ -0,0 +1,233 @@
+package uaa.melgozadelatorre.calcula.calculatorLayouts;
+
+public class Basic extends javax.swing.JPanel {
+
+    public Basic() {
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
+        jButton11 = new javax.swing.JButton();
+        jButton12 = new javax.swing.JButton();
+        jButton16 = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
+        jButton18 = new javax.swing.JButton();
+        jButton19 = new javax.swing.JButton();
+        jButton13 = new javax.swing.JButton();
+        jButton14 = new javax.swing.JButton();
+        jButton15 = new javax.swing.JButton();
+
+        java.awt.GridBagLayout layout = new java.awt.GridBagLayout();
+        layout.columnWidths = new int[] {0, 12, 0, 12, 0, 12, 0, 12, 0, 12, 0};
+        layout.rowHeights = new int[] {0, 10, 0, 10, 0, 10, 0, 10, 0};
+        setLayout(layout);
+
+        jButton1.setText("jButton1");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton1, gridBagConstraints);
+
+        jButton2.setText("jButton2");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton2, gridBagConstraints);
+
+        jButton3.setText("jButton3");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 6;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton3, gridBagConstraints);
+
+        jButton4.setText("jButton4");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 8;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton4, gridBagConstraints);
+
+        jButton5.setText("jButton5");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton5, gridBagConstraints);
+
+        jButton6.setText("jButton6");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton6, gridBagConstraints);
+
+        jButton7.setText("jButton7");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 6;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton7, gridBagConstraints);
+
+        jButton8.setText("jButton8");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 8;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton8, gridBagConstraints);
+
+        jButton9.setText("jButton9");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton9, gridBagConstraints);
+
+        jButton10.setText("jButton10");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton10, gridBagConstraints);
+
+        jButton11.setText("jButton11");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 6;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton11, gridBagConstraints);
+
+        jButton12.setText("jButton12");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 8;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton12, gridBagConstraints);
+
+        jButton16.setText("jButton16");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton16, gridBagConstraints);
+
+        jButton17.setText("jButton17");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton17, gridBagConstraints);
+
+        jButton18.setText("jButton18");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 6;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton18, gridBagConstraints);
+
+        jButton19.setText("jButton19");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 8;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.gridheight = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(2, 0, 1, 0);
+        add(jButton19, gridBagConstraints);
+
+        jButton13.setText("jButton13");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton13, gridBagConstraints);
+
+        jButton14.setText("jButton14");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton14, gridBagConstraints);
+
+        jButton15.setText("jButton15");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 6;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 46;
+        gridBagConstraints.insets = new java.awt.Insets(1, 0, 1, 0);
+        add(jButton15, gridBagConstraints);
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton11;
+    private javax.swing.JButton jButton12;
+    private javax.swing.JButton jButton13;
+    private javax.swing.JButton jButton14;
+    private javax.swing.JButton jButton15;
+    private javax.swing.JButton jButton16;
+    private javax.swing.JButton jButton17;
+    private javax.swing.JButton jButton18;
+    private javax.swing.JButton jButton19;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ MelgozaDeLaTorre/src/main/java/uaa/melgozadelatorre/calcula/calculatorLayouts/Complex.java
@@ -0,0 +1,25 @@
+package uaa.melgozadelatorre.calcula.calculatorLayouts;
+
+public class Complex extends javax.swing.JPanel {
+
+    public Complex() {
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 400, Short.MAX_VALUE));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 300, Short.MAX_VALUE));
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
